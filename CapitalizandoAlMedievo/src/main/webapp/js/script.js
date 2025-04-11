f575f7a452553d76b8cfd6efdd8facf5a11dd33a
Noni Beld <noecruzg@gmail.com>
2025-04-11 03:26:57 -0600

    moviendo el index

--- CapitalizandoAlMedievo/src/main/webapp/js/script.js
+++ CapitalizandoAlMedievo/src/main/webapp/js/script.js
@@ -3,3 +3,13 @@
  */
 
 console.log("Pagina Cargada correctamente.");
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Página cargada correctamente.");
+
+    const title = document.querySelector("h1");
+
+    setInterval(() => {
+        title.style.color = title.style.color === "red" ? "black" : "red";
+    }, 1000);
+});
